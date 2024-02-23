3ff40d9023604974bbaa6a9d51e89879ec2eaffd
Pavel <efremenkov.pn@yandex.ru>
2024-02-23 14:19:28 +0300

    messsage

---
+++ src/main/java/com/hack/selectel/SelectelApplication.java
@@ -0,0 +1,14 @@
+package com.hack.selectel;
+
+// import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+public class SelectelApplication {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("hello world");
+	}
+
+}
---
+++ src/test/java/com/hack/selectel/SelectelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hack.selectel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
